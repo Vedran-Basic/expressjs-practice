4ef13cbe02246e070d983708dc0aafba98c9c352
Vedran-Basic <vedran.basic98@gmail.com>
2020-03-02 05:57:17 +0100

    expressJS basics

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const logger = require('./middleware/logger');
+
+
+const app = express();
+
+//Init middleware
+// app.use(logger);
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public',  'index.html'));
+// });
+
+//body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//members API routes
+app.use('/api/members', require('./routes/api/members'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
